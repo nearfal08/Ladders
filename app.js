1f78b465d39b75972c205475b3b0b2d67476ef89
wes <wes@onehat.com>
2017-12-27 22:05:52 -0600

    Results now save to the db. Web page pulls from db instead of api.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var helmet = require('helmet');
 // League api wrapper
 var riotApi = require("riot-api-nodejs");
 const https = require("https"); 
+var mysql = require('mysql');
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 // fake posts to simulate a database
@@ -40,6 +41,7 @@ const posts = [
 
 // Use modules
 app.use(express.static(__dirname + '/node_modules'));
+app.use("/public", express.static(__dirname + "/public"));
 app.use(helmet())
 
 // Route the http request to the index file.
@@ -62,22 +64,25 @@ io.on('connection', function (client) {
     // When we recieve "messages" from the client, broadcast the data. 
     // Broadcast sends to all clients.
     client.on('get_challenger', function (data) {
-        //client.emit('broad', data);
-        //client.broadcast.emit('broad', data);
-        getChallengers(function(players) { 
-            var arrayPlayers = Object.keys(players.entries).map(function (key) {
-              return players.entries[key];
-            });
-            arrayPlayers.sort(function(a, b){
-                var keyA = new Date(a.leaguePoints),
-                    keyB = new Date(b.leaguePoints);
-                // Compare the 2 dates
-                if(keyA > keyB) return -1;
-                if(keyA < keyB) return 1;
-                return 0;
-            });
-            client.emit('challenger_players', arrayPlayers);
-            client.broadcast.emit('challenger_players', arrayPlayers);
+        var connection = dbConnection();
+        connection.connect(); 
+        getRankings(connection, function(players) { 
+            connection.end();   
+//            var arrayPlayers = Object.keys(players).map(function (key) {
+//              return players[key];
+//            });
+//            arrayPlayers.sort(function(a, b){
+//                var keyA = new Date(a.leaguePoints),
+//                    keyB = new Date(b.leaguePoints);
+//                // Compare the 2 dates
+//                if(keyA > keyB) return -1;
+//                if(keyA < keyB) return 1;
+//                return 0;
+//            });  
+            console.log(players);
+            
+            client.emit('challenger_players', players);
+            client.broadcast.emit('challenger_players', players);
              
         }); 
         // databaseLogTest(function(result) {
@@ -87,20 +92,26 @@ io.on('connection', function (client) {
     
     // NEEDS UPDATED. TIMER IS NOT CONSISTANT
     setInterval(function() {
-        getChallengers(function(players) { 
-            var arrayPlayers = Object.keys(players.entries).map(function (key) {
-              return players.entries[key];
-            });
-            arrayPlayers.sort(function(a, b){
-                var keyA = new Date(a.leaguePoints),
-                    keyB = new Date(b.leaguePoints);
-                // Compare the 2 dates
-                if(keyA > keyB) return -1;
-                if(keyA < keyB) return 1;
-                return 0;
-            });
-            client.emit('challenger_players', arrayPlayers);
-            client.broadcast.emit('challenger_players', arrayPlayers);
+        var connection = dbConnection();
+        connection.connect(); 
+        getRankings(connection, function(players) {   
+
+            connection.end(); 
+//            var arrayPlayers = Object.keys(players.entries).map(function (key) {
+//              return players.entries[key];
+//            });
+//            arrayPlayers.sort(function(a, b){
+//                var keyA = new Date(a.leaguePoints),
+//                    keyB = new Date(b.leaguePoints);
+//                // Compare the 2 dates
+//                if(keyA > keyB) return -1;
+//                if(keyA < keyB) return 1;
+//                return 0;
+//            });
+            console.log(players);
+            
+            client.emit('challenger_players', players);
+            client.broadcast.emit('challenger_players', players);
              
         });
     }, 30000); //time is in ms
@@ -109,37 +120,80 @@ io.on('connection', function (client) {
 // Tell server to listen on port ****.
 server.listen(4200);
 
-function databaseLogTest(callback) {
-    var mysql = require('mysql')
+function dbConnection() { 
     var connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: '',
         database: 'ladder'
-    });
+    }); 
+ 
+    return connection;
+}
 
-    connection.connect()
+function updateRankings(arrayPlayers) { 
+    // pull from api
+    //getApiRankings(function(players) {
+//            var arrayPlayers = Object.keys(players.entries).map(function (key) {
+//              return players.entries[key];
+//            });
+//            arrayPlayers.sort(function(a, b){
+//                var keyA = new Date(a.leaguePoints),
+//                    keyB = new Date(b.leaguePoints);
+//                // Compare the 2 dates
+//                if(keyA > keyB) return -1;
+//                if(keyA < keyB) return 1;
+//                return 0;
+//            });
+//    var connection = dbConnection();
+//    connection.connect();
+    
+//    wipeRankings(connection);
+//    storeRankings(connection, arrayPlayers);
+//
+//    connection.end();
+    //})
+ 
+ 
+}
 
-    //    connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
-    //      if (err) throw err
-    //
-    //      console.log('The solution is: ', rows[0].solution)
-    //    })
-    // Insert data
-    var post = {
-        name: 'Weston Walker'
-    };
-    connection.query('INSERT INTO node_test SET ?', post, function (error, results, fields) {
+function wipeRankings(connection) {
+    // Delete live rankings
+    connection.query('DELETE FROM lol_live_standings', [], function (error, results, fields) {
         if (error) throw error;
         console.log('Result: ', results);
-    });
+    });  
+}
 
-    connection.end()
-    callback(post);
-} 
+function storeRankings(connection, rankings) { 
+    for (var i = 0, len = rankings.length; i < len; i++) { 
+        var player = {
+            rank: i + 1,
+            name: rankings[i].playerOrTeamName,
+            points: rankings[i].leaguePoints, 
+            wins: rankings[i].wins,  
+            losses: rankings[i].losses
+        };
+        connection.query('INSERT INTO lol_live_standings SET ?', player, function (error, results, fields) {
+            if (error) throw error;
+            console.log('Result: ', results);
+        });  
+    }
+ 
+}
 
-function getChallengers(callback) {  
-    var url = 'https://na1.api.riotgames.com/lol/league/v3/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key=RGAPI-ba38d44e-2817-4646-b0c7-668989931337',
+function getRankings(connection, callback) {  
+    var rankings;
+    
+    connection.query("SELECT * FROM lol_live_standings ORDER BY rank ASC", function (err, result, fields) {
+        if (err) throw err;
+        rankings = result; 
+        callback(rankings)
+    });  
+}
+
+function getApiRankings(callback) {  
+    var url = 'https://na1.api.riotgames.com/lol/league/v3/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key=RGAPI-5d6fd823-8d80-4089-a08d-5529c3755d2c',
         result = null;
     https.get(url, res => {
       res.setEncoding("utf8");
